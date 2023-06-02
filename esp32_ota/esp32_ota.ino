f2936b2d7affb3cd5a0897277862040bfaab5bf2
B. NARONGGORN <naronggorn12@windowslive.com>
2023-06-02 20:18:50 +0700

    2.1 set test

--- esp32_ota/esp32_ota.ino
+++ esp32_ota/esp32_ota.ino
@@ -9,13 +9,11 @@ const char * password = "Nikk@229";
 
 
 String FirmwareVer = {
-  "2.2"
+  "2.1"
 };
-#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
+#define URL_fw_Version "https://github.com/naronggorn12/ESP32_OTATEST/blob/main/bin_version.txt"
+#define URL_fw_Bin "https://github.com/naronggorn12/ESP32_OTATEST/blob/main/fw.bin"
 
-//#define URL_fw_Version "http://cade-make.000webhostapp.com/version.txt"
-//#define URL_fw_Bin "http://cade-make.000webhostapp.com/firmware.bin"
 
 void connect_wifi();
 void firmwareUpdate();
